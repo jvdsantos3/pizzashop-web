11ef755c4f660be27ce11588745405a6de401a4e
João Vitor <jv.dossantos012@gmail.com>
2025-04-10 23:03:24 -0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <RouterProvider router={router} />
-    </HelmetProvider>
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
